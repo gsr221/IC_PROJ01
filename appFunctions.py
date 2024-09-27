d4cf17c43f1f54e95b4d547a3c9127f6a3469523
Gabriel Ribeiro <138245389+gsr221@users.noreply.github.com>
2024-09-27 17:22:29 -0300

    Add files via upload

--- appFunctions.py
+++ appFunctions.py
@@ -2,6 +2,7 @@ import pandas as pd
 from odFunctions import DSS
 import tkinter as tk
 import consts as c
+from modeloAg import AG
 
 perc = 2
 
@@ -9,18 +10,17 @@ class AppFunctions():
     def __init__(self):
         self.busFileLink = c.link_ieee13bus
         self.dss = DSS()
+        self.ag = AG()
     
     def clearData(self, tv):
         tv.delete(*tv.get_children())
         
-
     def calcDesqButtFun(self, tv):
         self.clearData(tv)
         dfSeqVoltages = self.dss.dfSeqVolt()
         if dfSeqVoltages.empty:
             tk.messagebox.showerror("Informativo", "Houve algum problema com o diretório do arquivo 'IEEE13Nodeckt_EXP_SEQVOLTAGES.CSV'; verifique o diretório no arquivo consts.py")
             return None
-        print(dfSeqVoltages.columns)
         dfSeqVoltages = dfSeqVoltages.drop(['  p.u.', 'Base kV',' %V0/V1', ' Vresidual', ' %NEMA'], axis=1)
         dfBarrasDeseq = dfSeqVoltages.loc[dfSeqVoltages[dfSeqVoltages.columns[3]] > perc]
         
@@ -37,10 +37,28 @@ class AppFunctions():
             
         return None
     
-    def equilButtFun(self, tv, entPma, entPmb, entPmc):
-        pms = [entPma.get(), entPmb.get(), entPmc.get()]
+    def equilButtFun(self, tv, entryPma, entryPmb, entryPmc):
+        self.clearData(tv)
+        pms = [entryPma.get(), entryPmb.get(), entryPmc.get()]
+        pms = [int(pm) for pm in pms]
         if '' in pms:
             tk.messagebox.showerror("Informativo", "Insira valores para Pma, Pmb e Pmc")
             return None
+        results, log, dicMelhoresIndiv = self.ag.execAg(pms=pms,
+                                                        cxpb=0.8,
+                                                        mutpb=0.1,
+                                                        ngen=400,
+                                                        numRep=1)
+        dfMelhoresIndiv = pd.DataFrame(dicMelhoresIndiv)
         
+        tv["column"] = list(dfMelhoresIndiv)
+        tv["show"] = "headings"
+        for column in tv["columns"]:
+            tv.heading(column, text=column)
+            
+        df_rows = dfMelhoresIndiv.to_numpy().tolist()
+        for row in df_rows:
+            tv.insert("", "end", values=row)
+
+        return None
         
--- modeloAg.py
+++ modeloAg.py
@@ -10,23 +10,26 @@ class AG():
         self.dss.compileFile(c.link_ieee13bus)
         self.barras = self.dss.BusNames()
         self.pmList = []
+       
         
-    def FOB (self, indiv):
+    def alocaPot(self, barramento, listaPoten):
+        self.dss.clearAll()
+        self.dss.compileFile(c.link_ieee13bus)
+        self.dss.dssCircuit.SetActiveBus(barramento)
+        kVBaseBarra = self.dss.dssBus.kVBase
+        for fase in range(len(listaPoten)):
+            comando = "New Load.NEW"+str(fase+1)+" Bus1="+str(barramento)+"."+str(fase+1)+" Phases=1 Conn=Wye Model=1 kV="+str(round(kVBaseBarra, 2))+" kW="+str(listaPoten[fase])+" kvar=0"
+            self.dss.dssTxt.Command = comando
+        
+        
+    def FOB(self, indiv):
         barra = indiv[-1]
         pots = indiv[:3]
         
-        if barra > len(self.barras)-1:
-            return 100
+        if barra > len(self.barras)-1 or barra < 0:
+            return 100, 
 
-        #Reseta o arquivo do openDSS
-        self.dss.clearAll()
-        self.dss.compileFile(c.link_ieee13bus)
-        self.dss.dssBus.SetActiveBus(self.barras[barra])
-        kVbaseBarra = self.dss.dssBus.kVBaseBus(self.barras[barra])
-
-        #Cria as novas cargas
-        for pot in range(len(pots)):
-            self.dss.dssTxt.Command = "New Load.NEW"+str(pot+1)+" Bus1="+str(self.barras[barra])+"."+str(pot+1)+"Phases=1 Conn=Wye Model=1 kV="+str(round(kVbaseBarra, 2))+" kW="+str(pots[pot])+" kvar=0"
+        self.alocaPot(barramento=self.barras[barra], listaPoten=pots)
         
         self.dss.solve(1)
         
@@ -42,35 +45,42 @@ class AG():
         barra = indiv[-1]
         pots = indiv[:3]
         
-        if barra > len(self.barras)-1:
-            return False
-        
-        self.dss.clearAll()
-        self.dss.compileFile(c.link_ieee13bus)
-        self.dss.dssBus.SetActiveBus(self.barras[barra])
-        kVbaseBarra = self.dss.dssBus.kVBaseBus(self.barras[barra])
+        if barra > len(self.barras)-1: return False
 
-        #Cria as novas cargas
-        for pot in range(len(pots)):
-            self.dss.dssTxt.Command = "New Load.NEW"+str(pot+1)+" Bus1="+str(self.barras[barra])+"."+str(pot+1)+"Phases=1 Conn=Wye Model=1 kV="+str(round(kVbaseBarra, 2))+" kW="+str(pots[pot])+" kvar=0"
+        if sum(pots) != 0: return False
+        
+        if any(pots[idx] > self.pmList[idx] for idx in range(len(pots))): return False
+        
+        self.alocaPot(barramento=self.barras[barra], listaPoten=pots)
         
         self.dss.solve(1)
-        
         dfSeqVoltages = self.dss.dfSeqVolt()
         dicSecVoltages = dfSeqVoltages.to_dict(orient = 'list')
         deseq = dicSecVoltages[' %V2/V1']
-        deseqState = all(x <= 2 for x in deseq)
-        potMaxState = all(pots[idx] <= self.pmList[idx] for idx in range(len(pots)))
-        somaPots = sum(pots)
-        if somaPots == 0: somaPotsState = True
-        else: somaPotsState = False
-        states = [deseqState, potMaxState, somaPotsState]
         
-        return all(state == True for state in states)
+        if any(x > 2 for x in deseq): return False
         
-    
+        return True
+
+        
     def penalidade (self, indiv):
-        return 50
+        barra = indiv[-1]
+        pots = indiv[:3]
+        
+        deltaSumPots = sum(pots)
+        deltaPots = (pots[idx]-self.pmList[idx] for idx in range(len(pots)))
+        
+        self.alocaPot(barramento=self.barras[barra], listaPoten=pots)
+        
+        self.dss.solve(1)
+        dfSeqVoltages = self.dss.dfSeqVolt()
+        dicSecVoltages = dfSeqVoltages.to_dict(orient = 'list')
+        deseq = dicSecVoltages[' %V2/V1']
+        deltaDeseq = (deseqVal-2 for deseqVal in deseq)
+        
+        deltaTot = deltaSumPots + sum(deltaPots) + sum(deltaDeseq)
+        
+        return deltaTot
         
         
     def mutateFun(self, indiv):
@@ -86,28 +96,44 @@ class AG():
             indiv[idx] = random.randint(0, len(self.barras))
     
     
-    def configAg(self, pms):
+    def criaCrom (self):
+        crom = [random.randint(-self.pmList[0], self.pmList[0]), 
+                random.randint(-self.pmList[1], self.pmList[1]), 
+                random.randint(-self.pmList[2], self.pmList[2]),
+                random.randint(0,12)]
+        
+        return crom
+    
+    def execAg(self, pms, cxpb, mutpb, ngen, numRep):
+        #Objeto toolbox
+        toolbox = base.Toolbox()
         #Lista com os valores de Potencia máxima por fase
         self.pmList = pms
         #Criando uma classe de Fitness minimizado
-        creator.create("fitnessMulti", base.Fitness, weights=(-1.0,))
+        creator.create("fitnessMulti", base.Fitness, weights=(-1.0, ))
         #Criando a classe do indivíduo
-        creator.create("estrIndiv", list, fitness=creator.FitnessMulti)
-        #Objeto toolbox
-        self.toolbox = base.Toolbox()
-        #Definindo um atributo que será um inteiro de -max até um valor +max
-        self.toolbox.register("gene", random.randint, -max(self.pmList), max(self.pmList))
-        #Definindo como criar um indivíduo (cromossomo) com 4 genes inteiros
-        self.toolbox.register("indiv", tools.initRepeat, creator.estrIndiv, self.toolbox.gene, n=4)
-        #Definindo a população
-        self.toolbox.register("pop", tools.initRepeat, list, self.toolbox.indiv)
-        #Criando uma população
-        pop = self.toolbox.pop(n=10)
-        #Definindo maneiras de cruzamento e de mutação
-        self.toolbox.register("mate", tools.cxOnePoint)
-        self.toolbox.register("mutate", self.mutateFun, indpb=0.1)
-        #Definindo o tipo de seleção
-        self.toolbox.register("select", tools.selTournament, tournsize=3)
-        #Definindo a fob e as restrições
-        self.toolbox.register("evaluate", self.FOB)
-        self.toolbox.register("evaluate", tools.DeltaPenalty(self.restricao, 1e6, self.penalidade))
+        creator.create("estrIndiv", list, fitness = creator.fitnessMulti)
+        dicMelhoresIndiv = {"cromossomos": [],
+                                "fobs": []}
+        
+        for _ in range(numRep):
+            #Definindo como criar um indivíduo (cromossomo) com 4 genes inteiros
+            toolbox.register("indiv", tools.initIterate, creator.estrIndiv, self.criaCrom)
+            #Definindo a população
+            toolbox.register("pop", tools.initRepeat, list, toolbox.indiv)
+            #Criando uma população
+            populacao = toolbox.pop(n=10)
+            #Definindo maneiras de cruzamento e de mutação
+            toolbox.register("mate", tools.cxOnePoint)
+            toolbox.register("mutate", self.mutateFun)
+            #Definindo o tipo de seleção
+            toolbox.register("select", tools.selTournament, tournsize=3)
+            #Definindo a fob e as restrições
+            toolbox.register("evaluate", self.FOB)
+            toolbox.register("evaluate", tools.DeltaPenalty(self.restricao, 1e6, self.penalidade))
+            hof = tools.HallOfFame(1)
+            result, log = algorithms.eaSimple(populacao, toolbox, cxpb=0.8, mutpb=0, ngen=100, halloffame=hof, verbose=False)
+            dicMelhoresIndiv["cromossomos"].append(hof[0])
+            dicMelhoresIndiv["fobs"].append(hof[0].fitness.values[0])
+        
+        return result, log, dicMelhoresIndiv
